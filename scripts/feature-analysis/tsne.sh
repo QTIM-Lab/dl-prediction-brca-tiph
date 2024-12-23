993618dea2e6ce4e08643399d07e264c1c3aa3b6
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-23 09:56:13 +0000

    Updated: scripts/feature-analysis/tsne.sh

--- scripts/feature-analysis/tsne.sh
+++ scripts/feature-analysis/tsne.sh
@@ -5,7 +5,7 @@
 # Environment Variables
 export PYENV_VERSION=mmxbrcp
 export LD_LIBRARY_PATH="/autofs/space/crater_001/tools/usr/lib64:${LD_LIBRARY_PATH}"
-export CUDA_VISIBLE_DEVICES=MIG-752466c4-c584-5e2c-9402-e840a3cf5e6f
+export CUDA_VISIBLE_DEVICES=GPU-a6f9db4e-0b70-8386-34d7-46f9a00b32e9
 
 
 
