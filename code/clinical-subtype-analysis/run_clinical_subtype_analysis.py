33adc9b7fe407a7cdcfdc6dcd2ca576187e6230d
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-23 16:41:33 +0000

    Added: code/clinical-subtype-analysis/

---
+++ code/clinical-subtype-analysis/run_clinical_subtype_analysis.py
@@ -0,0 +1,143 @@
+# Imports
+from __future__ import print_function
+import os
+import argparse
+import pandas as pd
+import numpy as np
+import random
+import json
+import copy
+import matplotlib.pyplot as plt
+
+# PyTorch Imports
+import torch
+
+# Project Imports
+from train_val_test_utilities import test_pipeline
+from data_utilities import TCGABRCA_MIL_DatasetClinicalSubtype
+
+
+
+# Function: See the seed for reproducibility purposes
+def set_seed(seed=42):
+
+    # Random Seed
+    random.seed(seed)
+
+    # Environment Variable Seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+    # NumPy Seed
+    np.random.seed(seed)
+
+    # PyTorch Seed(s)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+    return
+
+
+
+if __name__ == "__main__":
+
+    # CLI
+    parser = argparse.ArgumentParser(description='CLAM: Clinical Subtype Analysis after Inference.')
+    parser.add_argument('--gpu_id', type=int, default=0, help="The ID of the GPU we will use to run the program.")
+    parser.add_argument('--seed', type=int, default=42, help='Random seed for reproducibility (default: 42).')
+    parser.add_argument('--checkpoint_dir', type=str, required=True, help='The path to the checkpoint directory.')
+    parser.add_argument('--dataset', type=str, required=True, choices=['TCGA-BRCA'], help='The dataset for the experiments.')
+    parser.add_argument('--base_data_path', type=str, required=True, help='Base data path for TCGA-BRCA dataset.')
+    parser.add_argument('--experimental_strategy', type=str, choices=['All', 'DiagnosticSlide', 'TissueSlide'], required=True, help="The experimental strategy for the TCGA-BRCA dataset.")
+    parser.add_argument('--features_h5_dir', nargs='+', type=str, required=True, help="The directory of the features in .pt format for the TCGA-BRCA dataset.")
+    args = parser.parse_args()
+
+
+
+    # Set seeds (for reproducibility reasons)
+    set_seed(seed=args.seed)
+
+
+    # Load configuration JSON
+    with open(os.path.join(args.checkpoint_dir, "config.json"), 'r') as j:
+        config_json = json.load(j)
+
+
+    # Load GPU/CPU device
+    device = torch.device(f'cuda:{args.gpu_id}') if torch.cuda.is_available() else torch.device('cpu')
+
+
+    # Get the encoding size for the feature vectors
+    encoding_size = config_json['data']['encoding_size']
+
+    # Get verbose
+    verbose = config_json['verbose']
+
+    # Task type
+    if "task_type" in config_json.keys():
+        task_type = config_json["task_type"]
+    else:
+        task_type = "classification"
+        config_json["task_type"] = "classification"
+
+    
+    # Load data
+    print('Loading dataset...')
+    label = args.checkpoint_dir.split('/')[-2]
+    if args.dataset == 'TCGA-BRCA':
+        dataset = TCGABRCA_MIL_DatasetClinicalSubtype(
+            base_data_path=args.base_data_path,
+            experimental_strategy=args.experimental_strategy,
+            label=label,
+            features_h5_dir=args.features_h5_dir,
+            n_folds=int(config_json["data"]["n_folds"]),
+            seed=int(args.seed)
+        )
+
+
+        # Create the data splits from the original dataset
+        train_set = copy.deepcopy(dataset)
+        train_set.select_split(split='train')
+
+        val_set = copy.deepcopy(dataset)
+        val_set.select_split(split='validation')
+
+        test_set = copy.deepcopy(dataset)
+        test_set.select_split(split='test')
+
+
+    # Iterate through folds
+    n_folds = int(config_json["data"]["n_folds"])
+    for fold in range(n_folds):
+
+        # Set seed
+        set_seed(seed=args.seed)
+
+        if verbose:
+            print(f"Current Fold {fold+1}/{n_folds}")
+        
+
+        # Select folds in the database
+        train_set.select_fold(fold=fold),
+        val_set.select_fold(fold=fold),
+        test_set.select_fold(fold=fold)
+
+        for eval_set, eval_name in zip((train_set, val_set, test_set),('train', 'val', 'test')):
+            test_inference_info = test_pipeline(
+                test_set=eval_set,
+                config_json=config_json,
+                device=device,
+                checkpoint_dir=args.checkpoint_dir,
+                fold=fold
+            )
+
+            # Convert test metrics into a dataframe
+            test_inference_info_df = pd.DataFrame.from_dict(test_inference_info)
+            test_inference_info_df.to_csv(os.path.join(args.checkpoint_dir, f"{eval_name}_inference_info_kf{fold}.csv"))
+            os.makedirs(os.path.join('results', label), exist_ok=True)
+            test_inference_info_df.to_csv(os.path.join('results', f"{eval_name}_inference_info_kf{fold}.csv"))
+            print(eval_name)
+            print(test_inference_info_df)
---
+++ code/clinical-subtype-analysis/train_val_test_utilities.py
@@ -0,0 +1,173 @@
+# Imports
+import os
+import numpy as np
+
+# PyTorch Imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchinfo import summary
+
+# TorchMetrics Imports
+from torchmetrics.functional.classification import (
+    accuracy,
+    f1_score,
+    recall,
+    precision,
+    auroc
+)
+from torchmetrics.functional.regression import (
+    mean_absolute_error,
+    mean_squared_error,
+    concordance_corrcoef,
+    kendall_rank_corrcoef,
+    pearson_corrcoef,
+    r2_score,
+    relative_squared_error,
+    spearman_corrcoef
+)
+
+# Sklearn Imports
+# from sklearn.preprocessing import label_binarize
+# from sklearn.metrics import roc_auc_score, roc_curve, auc, accuracy_score
+
+# Project Imports
+from model_utilities import AM_SB, AM_MB, AM_SB_Regression
+
+# WandB Imports
+import wandb
+
+
+
+# Function: Get optimizer
+def get_optim(model, optimizer, lr, weight_decay):
+    if optimizer == "Adam":
+        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=lr, weight_decay=weight_decay)
+    elif optimizer == 'SGD':
+        optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=lr, momentum=0.9, weight_decay=weight_decay)
+    else:
+        raise NotImplementedError
+    return optimizer
+
+
+
+# Function: Test Model Pipeline
+def test_pipeline(test_set, config_json, device, checkpoint_dir, fold):
+
+    # Load the parameters from the configuration JSON
+    n_classes = config_json["data"]["n_classes"]
+    dropout = config_json["hyperparameters"]["dropout"]
+    dropout_prob = config_json["hyperparameters"]["dropout_prob"]
+    model_size = config_json["hyperparameters"]["model_size"]
+    model_type = config_json["hyperparameters"]["model_type"]
+    verbose = config_json["verbose"]
+    num_workers = config_json["data"]["num_workers"]
+    pin_memory = config_json["data"]["pin_memory"]
+    encoding_size = config_json['data']['encoding_size']
+    features_ = config_json["features"]
+    task_type = config_json["task_type"]
+
+
+    # Dictionary with model settings for the initialization of the model object
+    if task_type in ("classification", "clinical_subtype_classification"):
+        model_dict = {
+            "dropout":dropout,
+            "dropout_prob":dropout_prob,
+            'n_classes':n_classes,
+            "encoding_size":encoding_size
+        }
+    elif task_type == "regression":
+        model_dict = {
+            "dropout":dropout,
+            "dropout_prob":dropout_prob,
+            "encoding_size":encoding_size
+        }
+    
+
+    # Adapted from the CLAM framework
+    assert model_size is not None, "Please define a model size."
+    
+    model_dict.update({"size_arg": model_size})
+    
+    # AM-SB
+    if task_type in ("classification", "clinical_subtype_classification"):
+        if model_type == 'am_sb':
+            model = AM_SB(**model_dict)
+        elif model_type == 'am_mb':
+            model = AM_MB(**model_dict)
+    elif task_type == "regression":
+        if model_type == 'am_sb':
+            model = AM_SB_Regression(**model_dict)
+
+    if verbose:
+        print(f"Using features: {features_}")
+        summary(model)
+
+
+    # Create DataLoaders 
+    # FIXME: The code is currently optimized for batch_size == 1)
+    test_loader = DataLoader(
+        dataset=test_set,
+        batch_size=1,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=pin_memory
+    )
+
+    # Load model checkpoint
+    model.load_state_dict(torch.load(os.path.join(checkpoint_dir, f"best_model_kf{fold}.pt"), map_location=device))
+    model.to(device)
+
+    # Put model into evaluation 
+    model.eval()
+
+    # Initialize variables to track values
+    test_y_pred = list()
+    test_y_pred_proba = list()
+    test_y = list()
+
+    # Create a dictionary for test metrics
+    test_inference_info = dict()
+
+
+    # Initialize variables 
+    test_y_pred = list()
+    test_y_pred_proba = list()
+    test_y_pred_c = list()
+    test_y = list()
+    test_y_c = list()
+    test_y_cs = list()
+    case_ids = list()
+    svs_paths = list()
+
+    # Get batch of data
+    for _, input_data_dict in enumerate(test_loader):
+
+        features, ssgsea_scores, ssgsea_scores_bin, c_subtype, case_id, svs_path, = input_data_dict["features"].to(device), input_data_dict["ssgsea_scores"].to(device), input_data_dict["ssgsea_scores_bin"].to(device), input_data_dict["c_subtype"].to(device), input_data_dict["case_id"].to(device), input_data_dict["svs_path"].to(device)
+        output_dict = model(features)
+        logits = output_dict['logits']
+        y_pred = torch.where(logits > 0, 1.0, 0.0)
+        y_pred_proba = F.sigmoid(logits)
+        test_y_pred_c.extend(list(logits.squeeze(0).cpu().detach().numpy()))
+        test_y_pred.extend(list(y_pred.squeeze(0).cpu().detach().numpy()))
+        test_y.extend(list(ssgsea_scores_bin.cpu().detach().numpy()))
+        test_y_c.extend(list(ssgsea_scores.cpu().detach().numpy()))
+        test_y_pred_proba.extend(list(y_pred_proba.squeeze(0).cpu().detach().numpy()))
+        test_y_cs.extend(list(c_subtype.cpu().detach().numpy()))
+        case_ids.extend(list(case_ids.cpu().detach().numpy()))
+        svs_paths.extend(list(svs_path.cpu().detach().numpy()))
+
+
+    # Test inference information
+    test_inference_info["case_id"] = case_ids
+    test_inference_info["svs_path"] = svs_paths
+    test_inference_info["ssgsea_c"] = test_y_c
+    test_inference_info["ssgsea_b"] = test_y
+    test_inference_info["ssgsea_c_pred"] = test_y_pred_c
+    test_inference_info["ssgsea_b_pred"] = test_y_pred
+    test_inference_info["ssgsea_b_pred_proba"] = test_y_pred_proba
+    test_inference_info["c_subtype"] = test_y_cs
+
+    return test_inference_info
