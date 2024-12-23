b657bd0baf73d786f70a76d9fb09fcc4c2d529ac
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2024-12-23 12:12:12 +0000

    Updated: code/feature_analysis/conch/umap.py

--- code/feature_analysis/conch/umap.py
+++ code/feature_analysis/conch/umap.py
@@ -13,7 +13,7 @@ import torch
 from torch.utils.data import DataLoader
 
 # UMAP Imports
-import umap
+import umap.umap_ as umap
 
 # Project Imports
 from data_utilities import TCGABRCA_MIL_DatasetRegression
