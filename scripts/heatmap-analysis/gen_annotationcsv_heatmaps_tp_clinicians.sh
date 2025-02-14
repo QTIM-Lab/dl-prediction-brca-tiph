ca129a7e4529eb77f1b84373a516d3da6e03ecc9
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-02-14 11:45:30 +0000

    Updated: scripts/heatmap-analysis/gen_annotationcsv_heatmaps_tp_clinicians.sh

--- scripts/heatmap-analysis/gen_annotationcsv_heatmaps_tp_clinicians.sh
+++ scripts/heatmap-analysis/gen_annotationcsv_heatmaps_tp_clinicians.sh
@@ -4,7 +4,6 @@ echo 'Generating Global Annotation CSV file...'
 
 
 
-
 # All Tasks
 # TASKS=('b_cell_proliferation' \
 #  't_cell_mediated_cytotoxicity' \
@@ -18,13 +17,10 @@ echo 'Generating Global Annotation CSV file...'
 # TASKS=('t_cell_mediated_cytotoxicity')
 
 # Angiogenesis
-TASKS=('angiogenesis')
+# TASKS=('angiogenesis')
 
-
-# T-Cell Cytotoxicity, Angiogenesis, Epithelial-Mesenchymal Transition
-# CHECKPOINT_DIRS=(
- # 
- # '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/All/checkpoints/hallmark_epithelial_mesenchymal_transition/2024-04-25_11-05-38')
+# Epithelial Mesenchymal Transition
+TASKS=('epithelial_mesenchymal_transition')
 
 
 
