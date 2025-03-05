ef9d3c4d57f1f6efeeb3bc646381c9d00af657f5
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-03-05 23:09:42 +0000

    Updated: code/preprocessing/patch_and_segmentation/wsi_colour_analysis.py, scripts/wsi_colour_analysis.sh

--- scripts/wsi_colour_analysis.sh
+++ scripts/wsi_colour_analysis.sh
@@ -3,5 +3,5 @@
 
 
 python code/preprocessing/patch_and_segmentation/wsi_colour_analysis.py \
- --features_h5_dir '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CONCH/TCGA-BRCA/mmxbrcp/DiagnosticSlide/SegmentationHistoQC/features' '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CONCH/TCGA-BRCA/mmxbrcp/TissueSlide/SegmentationHistoQC/features' \
+ --features_h5_dir '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/DiagnosticSlide/SegmentationHistoQC' '/autofs/space/crater_001/projects/breast-cancer-pathology/results/CLAM/TCGA-BRCA/mmxbrcp/TissueSlide/SegmentationHistoQC' \
  --verbose
