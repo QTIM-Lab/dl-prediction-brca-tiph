433319cfdd93b37db3ab1500d19c1c2f52e80d85
TiagoFilipeSousaGoncalves <tiago.galves@hotmail.com>
2025-03-05 17:18:29 +0000

    Updated: code/, and scripts/

--- scripts/wsi_stats_analysis.sh
+++ scripts/wsi_stats_analysis.sh
@@ -2,7 +2,7 @@
 
 
 
-python code/models/clam/inference_model_fp.py \
+python code/preprocessing/patch_and_segmentation/wsi_stats_analysis.py \
  --base_data_path '/autofs/space/crater_001/datasets/public/TCGA-BRCA' \
  --use_histoqc_quality_file '/autofs/space/crater_001/projects/breast-cancer-pathology/results/HistoQC/TCGA-BRCA/mmxbrcp/hqc_quality_file.csv' \
  --verbose
